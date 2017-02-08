0c572d4c711489d03f0b59a2a653822e6fa16381
enotikk <asazanova@edu.hse.ru>
2017-02-08 11:43:11 +0400

    h/w 07.02.2017 - Исправлен вывод

--- utc.py
+++ utc.py
@@ -2,14 +2,18 @@ import re
 def utc(fname):
     f = open(fname, 'r', encoding='utf-8')
     s = f.read()
-    print(s)
-    regex = '"UTC.*"'
+    regex = '"(UTC.*?)"'
     r = re.search(regex, s)
+    f1 = open('result_'+fname, 'w', encoding='utf-8')
+    regex = 'PageName":"(.*?)"'
+    title = re.search(regex, s)
+    title1 = re.sub('_', ' ', title.group(1)) 
     if r != None:
-        print(r.group())
+        f1.write('Часовой пояс в городе '+ title1+' '+ r.group(1)+'.')
     else:
-        print('I did not find:(')
+        f1.write('I did not find:(')
     
 def main():
     fname = input('Введите название файла: ')
+    utc(fname)
 main()
